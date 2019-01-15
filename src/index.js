d37358778e48403679d148728b8440e7bd01553f
Bhargavi Devireddy <subbaru@gmail.com>
2019-01-15 21:00:37 +0000

    end of day2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 import './styles.css';
-console.log('Ready to learn some stufff!');
+import { getRandomInt, ready } from "./utils";
+
+let secretNumber,
+    squares;
+
+function doIt() {
+//find all the squares.
+ squares = document.querySelectorAll('.square');
+//on each of them, have something happen when they click
+let squareNumber = 1;
+
+squares.forEach(function(square){
+    square.addEventListener('click',handleClick);
+    square.dataset.number = squareNumber.toString();
+    squareNumber++;
+});
+
+//get a random number.
+secretNumber = getRandomInt(1,6);
+console.log('The Secret is',secretNumber);
+}
+function handleClick(evt) {
+    if(parseInt(this.dataset.number)===secretNumber)
+    {
+       this.classList.add('winner');
+       const currentSquare = this;                   
+       squares.forEach(function(square){
+         //remove click handlers from everything
+         square.removeEventListener('click',handleClick);
+         //make non-winners grey
+         if(square !== currentSquare)
+         {
+            square.classList.add('loser');
+         }
+        
+       })
+       
+    }
+
+    else
+    {
+        this.classList.add('loser');
+    }
+}
+ready(doIt);
